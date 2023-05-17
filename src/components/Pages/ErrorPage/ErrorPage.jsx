ca5927dcdf7d236490edf3b18d32ae773470ebff
protapsaha1 <webprotap09@gmail.com>
2023-05-18 02:16:52 +0600

    create error page

--- src/components/Pages/ErrorPage/ErrorPage.jsx
+++ src/components/Pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,12 @@
-
+import error from '../../../assets/images/404-error-page-svg-animation.gif';
 const ErrorPage = () => {
+    const handleBack = () => {
+        window.history.back();
+    };
     return (
         <div>
-            
+            <img className='mx-auto mt-20' src={error} alt="" />
+            <button className="btn btn-active btn-accent my-20 ms-96" onClick={handleBack}>Go Back</button>
         </div>
     );
 };
--- src/components/Pages/Navbar/Navbar.jsx
+++ src/components/Pages/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
             <div className="lg:max-w-screen-xl lg:mx-auto flex justify-between items-center py-2">
                 {/* website name */}
                 <div>
-                    <h1 className="text-pink-500 text-3xl font-bold font-serif">Kids Paradise</h1>
+                    <Link to="fkdkf"><h1 className="text-pink-500 text-3xl font-bold font-serif">Kids Paradise</h1></Link>
                 </div>
                 {/* routes */}
                 <div className="flex items-center">
@@ -26,7 +26,10 @@ const Navbar = () => {
                         <div className="input-group">
                             <input type="search" name="" placeholder="Search…" className="input input-bordered text-white" />
                             <button className="btn btn-square">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round"
+                                        strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                                </svg>
                             </button>
                         </div>
                     </div>
